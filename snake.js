9fbb9c0f4ce3adc04b39dc7948829b9dc012a3b0
munrhalls <antarcticdepths@gmail.com>
2021-03-03 21:14:59 +0100

    save progress

--- snake.js
+++ snake.js
@@ -1,30 +1,42 @@
 const grid = document.getElementsByTagName('main')[0];
 console.log(grid);
 
-
-
 function dominoGrid() {
-  dominoSquare.count = 0;
-function dominoSquare() {
-  let row = document.createElement('div');
-  row.style.display = 'flex'
-  for (let i = 0; i < 16; i++) {
-    let square = document.createElement('div');
-    square.style.border = '1px solid white';
+  dominoSquare.row = 1;
+  dominoSquare.num = 1;
 
-    square.style.backgroundColor = 'black';
-    square.style.display = 'inline-block';
-    square.style.width = '12px';
-    square.style.height = '12px';
-    row.appendChild(square);
+  function dominoSquare() {
+    dominoSquare.col = 1;
+    let row = document.createElement('div');
+    row.style.display = 'flex'
+    for (let i = 1; i <= 16; i++) {
+      let square = document.createElement('div');
+      square.style.border = '1px solid white';
+      square.style.backgroundColor = 'black';
+      square.style.display = 'inline-block';
+      square.style.width = '12px';
+      square.style.height = '12px';
+      row.appendChild(square);
+      square.id = dominoSquare.num + 1;
+      console.log(square.id)
+      dominoSquare.num ++;
+      square.className = 'square';
+      dominoSquare.col++;
+    }
+    grid.appendChild(row);
+    dominoSquare.row++;
+    if (dominoSquare.row < 16) {
+      dominoSquare();
+    }
   }
-  grid.appendChild(row);
-  dominoSquare.count++;
-  if (dominoSquare.count < 16) {
-    dominoSquare();
-  }
+  dominoSquare();
 }
-dominoSquare();
-}
+
+
+var x = document.getElementById('124');
+x.style.background = 'green';
+
+var squares = document.getElementsByClassName('square'); 
+
 
 dominoGrid();
