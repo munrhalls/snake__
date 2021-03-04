2170dc39ea8fa544d5f68078da21bbd2652cce39
munrhalls <antarcticdepths@gmail.com>
2021-03-04 18:07:12 +0100

    refactor

--- snake.js
+++ snake.js
@@ -1,33 +1,24 @@
-const grid = document.getElementsByTagName('main')[0];
+import styleSquare from './styles.js';
 
-let rowNum = 1;
-function dominoSquare() {
-  let row = document.createElement('div');
-  row.style.display = 'flex'
-  for (let i = 1; i <= 16; i++) {
-    let square = createSquare();
-    square.id = rowNum + ',' + i;
-    row.appendChild(square);
+function grid() {
+  const grid = document.getElementsByTagName('main')[0];
+  let rowNum = 1;
+  function dominoSquares() {
+    let row = document.createElement('div');
+    row.style.display = 'flex'
+    for (let i = 1; i <= 16; i++) {
+      let square = document.createElement('div');
+      styleSquare(square)
+      square.id = rowNum + ',' + i;
+      row.appendChild(square);
+    }
+    grid.appendChild(row);
+    rowNum++;
+    if (rowNum < 16) { 
+      dominoSquares(); 
+    }
   }
-  grid.appendChild(row);
-  rowNum++;
-
-  if (rowNum < 16) {
-    dominoSquare();
-  }
-}
-dominoSquare();
-
-function createSquare() {
-  let square = document.createElement('div');
-  square.style.border = '1px solid white';
-  square.style.backgroundColor = 'black';
-  square.style.display = 'inline-block';
-  square.style.width = '12px';
-  square.style.height = '12px';
-  return square;
+  dominoSquares();
 }
 
-var squares = document.getElementsByClassName('square');
-
-dominoSquare();
+grid();
---
+++ styles.js
@@ -0,0 +1,9 @@
+function styleSquare(square) {
+  square.style.border = '1px solid white';
+  square.style.backgroundColor = 'black';
+  square.style.display = 'inline-block';
+  square.style.width = '12px';
+  square.style.height = '12px';
+}
+
+export default styleSquare;
