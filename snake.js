3bc0839214fddc0f0ca46e14c3e67e57d959cb7d
munrhalls <antarcticdepths@gmail.com>
2021-03-04 20:19:51 +0100

    SNAKE DIRECTION: 1) add event listener, 2) add conditionals, 3) change active variable in each condition, correspondigly to arrow pressed

--- snake.js
+++ snake.js
@@ -9,7 +9,7 @@ import grid from './grid.js';
 //mark each square rownum, num
 // snake = 
 ////////// body - [array of ids] - 1) corresponding elems are accessed, 2) styled
-////////// direction - one state of 4: [r+ (top), r-(bot), col-(left), col+(right)] - 1) create active direction variable, 2) assign r+(top) to it, 3) create event listener, 4) associate each arrow key to corresponding direction, 5) upon hitting an arrow key, active direction variable changes, 6) 
+////////// direction - one state of 4: [r+ (top), r-(bot), col-(left), col+(right)] - 1) create active direction variable, 2) assign r+(top) to it, 3) create event listener, 4) associate each arrow key to corresponding direction, 5) upon hitting an arrow key, active direction variable changes
 ////////// movement - (move is func run at interval every sec) 1) deletes last el [array of ids]; 1) styles it back to normal, 2) creates new el, 3) checks first el [array of ids] and modifies it by [active direction], 4) adds that el to the snake body [array of ids]
 
 // 1) loop 
@@ -24,15 +24,37 @@ function snakeBody() {
     squareAtId.style.background = snakeColor;
   }
 }
-
 function snakeDirection() {
-  // this takes [num, num]
-  // and
-  // top -> [num-, num]
-  // right -> [num, num+]
-  // bot -> [num+, num]
-  // left -> [num, num-]
-  const = []
+  let activeDirection = 'TOP';
+  document.onkeydown = checkKey;
+  function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '38') {
+      activeDirection = 'TOP';
+    }
+    else if (e.keyCode == '40') {
+      activeDirection = 'DOWN';
+    }
+    else if (e.keyCode == '37') {
+      activeDirection = 'LEFT';
+    }
+    else if (e.keyCode == '39') {
+      activeDirection = 'RIGHT';
+    }
+  }
 }
+
+// snake movement
+// create function 
+// this takes [num, num]
+// and
+// top -> [num-, num]
+// right -> [num, num+]
+// bot -> [num+, num]
+// left -> [num, num-]
+
+// [num, num] -> [num-, num]
+// [num, num] -> num1 num2 -> num1 -1 -> return
+
 grid();
 snakeBody();
