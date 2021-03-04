b43d10806f6a78365272b82e4311edbd3adc3d4c
munrhalls <antarcticdepths@gmail.com>
2021-03-04 19:58:29 +0100

    1) snakeBody - corresponding id squares are accessed; 2) styled; DONE

--- snake.js
+++ snake.js
@@ -11,7 +11,20 @@ import grid from './grid.js';
 ////////// body - [array of ids] - 1) corresponding elems are accessed, 2) styled, 3) elements going out are restyled to normal
 ////////// direction - one state of 4: [r+ (top), r-(bot), col-(left), col+(right)]
 ////////// movement - (move is func run at interval every sec) 1) deletes last el [array of ids], 2) creates new el, 3) checks first el [array of ids] and modifies it by [active direction], 4) adds that el to the snake body [array of ids]
-// loop
+
+// 1) loop 
+// 1) access element
+// 1) style it
+
+function snakeBody() {
+  let snakeBody = ['11,12'];
+  let snakeColor = 'green';
+  for (let i = 0; i < snakeBody.length; i++) {
+    let squareAtId = document.getElementById(snakeBody[i]);
+    console.log(squareAtId)
+    squareAtId.style.background = snakeColor;
+  }
+}
 
 grid();
-
+snakeBody();
