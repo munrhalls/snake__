ac759cc6bc7fabf950b4ebd85a92fefeb6dfadac
munrhalls <antarcticdepths@gmail.com>
2021-03-08 22:16:34 +0100

    snake works again

--- snake.js
+++ snake.js
@@ -1,97 +1,102 @@
 import grid from './grid.js';
 const gridBg = 'white';
-// game
-// grid
-// food
-// snake
+
+// game - just runs/pauses/resumes other functions
+// snake 
+// on each game frame
+// gather current snake area
+// paint current snake area 
+// update snake area
+// establish direction
+// properly construct new id
+// unshift that new id to first element
+// delete last element
+// paint snake area
+
+
 function game() {
   game.time = 0;
   game.running = true;
-  game.pause = function() {
+  game.pause = function () {
     game.running = false;
   }
-  game.resume = function() {
+  game.resume = function () {
     game.running = true;
   }
+  grid(gridBg);
   function run() {
-    console.log('running')
+    snakeMove();
   }
-  function timer() {
+  function gameFrame() {
     setTimeout(function () {
       game.time++;
-      run();
 
       if (game.running) {
-        timer();
+        gameFrame();
+        run();
       }
     }, 1000);
 
   }
-  timer();
+  gameFrame();
+
+  let snakeBody = ['19,19'];
+  let snakeColor = 'green';
+  var activeDirection = 'TOP';
+  document.onkeydown = checkKey;
+
+  function checkKey(e) {
+    e = e;
+    if (e.keyCode == '38') {
+      activeDirection = 'TOP';
+    }
+    else if (e.keyCode == '40') {
+      activeDirection = 'BOTTOM';
+    }
+    else if (e.keyCode == '37') {
+      activeDirection = 'LEFT';
+    }
+    else if (e.keyCode == '39') {
+      activeDirection = 'RIGHT';
+    }
+    else if (e.keyCode == '32') {
+      snake.pause();
+    }
+    console.log(activeDirection);
+  }
+  function snakeMove() {
+    var vert = snakeBody[0].split(',')[0];
+    var horiz = snakeBody[0].split(',')[1];
+    if (activeDirection === 'TOP') {
+      vert--;
+    } else if (activeDirection === 'BOTTOM') {
+      vert++;
+    } else if (activeDirection === 'RIGHT') {
+      horiz++;
+    } else if (activeDirection === 'LEFT') {
+      horiz--;
+    }
+
+    console.log(vert);
+
+    let newCoord = vert + ',' + horiz;
+    snakeBody.unshift(newCoord);
+    let newSquare = document.getElementById(newCoord);
+
+    if (newSquare.className !== 'food') {
+      let lastCoord = snakeBody[snakeBody.length - 1];
+      let lastSnakeEl = document.getElementById(lastCoord);
+      lastSnakeEl.style.background = gridBg;
+      snakeBody.pop();
+    }
+    for (let i = 0; i < snakeBody.length; i++) {
+      let id = snakeBody[i];
+      let squareAtId = document.getElementById(id);
+      squareAtId.style.background = snakeColor;
+    }
+  }
 }
 game();
 game.pause();
 game.resume();
 
-
-// function snake() {
-//   let snakeBody = ['19,19'];
-//   let snakeColor = 'green';
-//   var activeDirection = 'TOP';
-//   document.onkeydown = checkKey;
-
-//   function checkKey(e) {
-//     e = e;
-//     if (e.keyCode == '38') {
-//       activeDirection = 'TOP';
-//     }
-//     else if (e.keyCode == '40') {
-//       activeDirection = 'BOTTOM';
-//     }
-//     else if (e.keyCode == '37') {
-//       activeDirection = 'LEFT';
-//     }
-//     else if (e.keyCode == '39') {
-//       activeDirection = 'RIGHT';
-//     }
-//     else if (e.keyCode == '32') {
-//       snake.pause();
-//     }
-//     console.log(activeDirection);
-//   }
-
-//   function snakeMove() {
-//     var vert = snakeBody[0].split(',')[0];
-//     var horiz = snakeBody[0].split(',')[1];
-//     if (activeDirection === 'TOP') {
-//       vert--;
-//     } else if (activeDirection === 'BOTTOM') {
-//       vert++;
-//     } else if (activeDirection === 'RIGHT') {
-//       horiz++;
-//     } else if (activeDirection === 'LEFT') {
-//       horiz--;
-//     }
-//     console.log(vert)
-//     let newCoord = vert + ',' + horiz;
-//     snakeBody.unshift(newCoord);
-//     let newSquare = document.getElementById(newCoord);
-
-//     if (newSquare.className !== 'food') {
-//       let lastCoord = snakeBody[snakeBody.length - 1];
-//       let lastSnakeEl = document.getElementById(lastCoord);
-//       lastSnakeEl.style.background = gridBg;
-//       snakeBody.pop();
-//     }
-//     for (let i = 0; i < snakeBody.length; i++) {
-//       let id = snakeBody[i];
-//       let squareAtId = document.getElementById(id);
-//       squareAtId.style.background = snakeColor;
-//     }
-//   }
-//   snakeMove();
-// }
-
-// grid(gridBg);
-// food(2250);
-// snake();
